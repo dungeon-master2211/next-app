20cc2c0c3a09299a75604e7bb9b147c9fe7a2de8
unknown <harshit.sri2211@gmail.com>
2025-04-20 16:37:54 +0530

    add prisma and postgres to save user info

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,16 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next"
-
+import { PrismaClient } from "../../generated/prisma/client"
 interface Signup{
     name:string
     email:string
     password:string
 }
-
+const client = new PrismaClient()
 export async function POST(req:Request,res:NextApiResponse){
     try{
         console.log(req)
         const body = await req.json()
-        console.log(body)
+        // Store body in db
+        await client.user.create({
+            data:{
+                name:body.name,
+                email:body.email,
+                password:body.password
+            }
+        })
         return Response.json({message:'User added successfully!'})
     }catch(e){
         console.log(e)
